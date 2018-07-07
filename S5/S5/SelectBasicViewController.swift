c70196fd2701fc34b54e9afda25ce74219e204db
Toshiki Ikda <b1013001@fun.ac.jp>
2018-07-07 23:47:15 +0900

    ローカルでリスト表示できた

--- S5/S5/SelectBasicViewController.swift
+++ S5/S5/SelectBasicViewController.swift
@@ -8,7 +8,13 @@
 
 import UIKit
 
-class SelectBasicViewController: UIViewController {
+class SelectBasicViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    /// 画像のファイル名
+    let imageNames = ["souce/souce1.png", "souce/souce2.png", "souce/souce3.png", "souce/souce4.png"]
+    
+    /// 画像のタイトル
+    let imageTitles = ["ソース1", "ソース2", "ソース3", "ソース4"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +25,23 @@ class SelectBasicViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    /// セルの個数を指定するデリゲートメソッド（必須）
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return imageNames.count
+    }
+    
+    /// セルに値を設定するデータソースメソッド（必須）
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // セルを取得
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell") as! CustomTableViewCell
+        
+        // セルに値を設定
+        cell.myImageView.image = UIImage(named: imageNames[indexPath.row])
+        cell.myTitleLabel.text = imageTitles[indexPath.row]
+        
+        return cell
     }
     
 
