dbca02cf1ae26c0dc70edb166be8a3d7d0b50310
Toshiki Ikda <b1013001@fun.ac.jp>
2018-07-07 22:01:29 +0900

    画面一通り用意

---
+++ S5/S5/SelectOtherViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SelectOtherViewController.swift
+//  S5
+//
+//  Created by 池田俊輝 on 2018/07/07.
+//  Copyright © 2018年 manji. All rights reserved.
+//
+
+import UIKit
+
+class SelectOtherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
