c74b2251366d04cf6c674f71c465007d2644b65e
Toshiki Ikda <b1013001@fun.ac.jp>
2018-07-07 21:07:18 +0900

    プロジェクト作成

---
+++ S5/S5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  S5
+//
+//  Created by 池田俊輝 on 2018/07/07.
+//  Copyright © 2018年 manji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
